9063f582fc482eecb8a4362534d5ac01ca92e705
Mina Fayez <MinaFayezN@gmail.com>
2023-07-24 13:00:53 +0300

    Session 04
    - Android Studio intro

---
+++ app/src/main/java/dev/mina/internship/ui/theme/Operations.kt
@@ -0,0 +1,20 @@
+package dev.mina.internship.ui.theme
+
+fun sumTwoNumbers(fNumber: Int, sNumber: Int): Int {
+    // heavy operation
+sumNumbers()
+
+    return fNumber  - sNumber
+}
+//
+/*
+
+ */
+/**
+ *
+ */
+@Deprecated("please use x function instead")
+fun sumNumbers(vararg numbers: Int): Int {
+    // heavy operation
+    return numbers.sum()
+}
---
+++ app/src/test/java/dev/mina/internship/ui/theme/OperationsKtTest.kt
@@ -0,0 +1,16 @@
+package dev.mina.internship.ui.theme
+
+import org.junit.Test
+
+
+internal class OperationsKtTest{
+
+
+    @Test
+    fun testSumNumbers(){
+
+        val result = sumTwoNumbers(1,2)
+        assert(result == 3)
+
+    }
+}
