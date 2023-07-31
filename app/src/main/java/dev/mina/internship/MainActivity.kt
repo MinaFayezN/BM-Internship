6a7b731a74fb1c174d1133d0baca6c72701124ce
Mina Fayez <MinaFayezN@gmail.com>
2023-07-31 13:02:20 +0300

    Session 09
    - Compose Components 03

--- app/src/main/java/dev/mina/internship/MainActivity.kt
+++ app/src/main/java/dev/mina/internship/MainActivity.kt
@@ -3,13 +3,27 @@ package dev.mina.internship
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.mina.internship.ui.theme.BMInternshipTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +31,77 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BMInternshipTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+
+                ProfileCard()
+
+                val state = rememberScrollState()
+                Column(modifier = Modifier.verticalScroll(state),) {
+
                 }
             }
+
+            // show list of 20 items
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+
+data class Profile (val name:String,val job :String)
+
+
+fun getDummyProfiles(): List<Profile> {
+    return listOf(
+        Profile("John", "Software Engineer"),
+        Profile("Alice", "Designer"),
+        Profile("Bob", "Data Scientist"),
+        Profile("Emily", "Product Manager"),
+        Profile("Michael", "Marketing Specialist"),
+        Profile("Olivia", "Teacher"),
+        Profile("David", "Doctor"),
+        Profile("Sophia", "Accountant"),
+        Profile("Daniel", "Sales Manager"),
+        Profile("Emma", "HR Manager"),
+        Profile("William", "Graphic Designer"),
+        Profile("Ava", "Lawyer"),
+        Profile("James", "Chef"),
+        Profile("Isabella", "Architect"),
+        Profile("Alexander", "Financial Analyst"),
+        Profile("Mia", "Journalist"),
+        Profile("Ethan", "Mechanical Engineer"),
+        Profile("Charlotte", "Nurse"),
+        Profile("Oliver", "Photographer"),
+        Profile("Amelia", "Researcher")
     )
 }
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BMInternshipTheme {
-        Greeting("Android")
+fun ProfileCard() {
+    Card(
+        elevation = CardDefaults.cardElevation(12.dp),
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            // Avatar Image
+            Image(
+                painter = painterResource(id = R.drawable.user),
+                contentDescription = "Profile Avatar",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Name and Description
+            Text(
+                text = "John Doe",
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            )
+            Text(
+                text = "Software Engineer",
+                style = TextStyle(fontSize = 14.sp)
+            )
+        }
     }
 }
