8b1020fb3add08b1634490840d0024a03f205db2
Mina Fayez <MinaFayezN@gmail.com>
2023-08-01 09:09:16 +0300

    Session 09
    - Compose Components 03

---
+++ app/src/main/java/dev/mina/internship/ui/screens/details/DetailsScreen.kt
@@ -0,0 +1,11 @@
+package dev.mina.internship.ui.screens.details
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailsScreen() {
+
+
+}
